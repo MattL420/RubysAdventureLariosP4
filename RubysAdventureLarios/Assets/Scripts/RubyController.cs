9ce812e5039a879ee2788bb52658c6712310fa31
MattL420 <116380725+MattL420@users.noreply.github.com>
2022-12-08 11:43:49 -0800

    Added things
    Added trees, fixed some scripts, and etc.

--- RubysAdventureLarios/Assets/Scripts/RubyController.cs
+++ RubysAdventureLarios/Assets/Scripts/RubyController.cs
@@ -7,24 +7,16 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //create 2 variables to use the unity built in axes
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-
-        //Create my movement vector variable
         Vector2 position = transform.position;
-
-        //Create the movement of my character
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-
-        //Set the new position
+        position.x = position.x + 0.1f * horizontal * Time.deltaTime;
+        position.y = position.y + 0.1f * vertical * Time.deltaTime;
         transform.position = position;
 
     }
